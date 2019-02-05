91a537b5a170ca336edc28d642ee1ee0ee811ca0
Greg K <greg.kitchen@cvsl.us.com>
2019-02-05 08:43:50 -0600

    Initial commit for M/R example

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Test
+        List<int> inputList = new List<int> { 0, 1, 2, 3 };
+        List<int> mappedList = new List<int> { 1, 2, 3, 4 };
+
+        Func<int, int> mapFunc = a => a + 1;
+        Func<int, int, int> reduceFunc = (a, b) => a + b;
+
+        bool mapWorks = map(inputList, mapFunc).SequenceEqual(mappedList);
+        bool reduceWorks = reduce(inputList, reduceFunc, 4) == 10;
+
+        Console.WriteLine("Running tests");
+        Console.WriteLine("Reduce works: " + reduceWorks);
+        Console.WriteLine("Map works: " + mapWorks);
+    }
+
+    // use for(each) loops to implement below functions
+    public static List<B> map<A, B>(List<A> list, Func<A, B> func)
+    {
+        List<B> bList = new List<B>();
+        list.ForEach((x) => bList.Add(func(x)));
+        return bList;
+    }
+
+    public static B reduce<A, B>(List<A> list, Func<B, A, B> func, B initial)
+    {
+        bool measure = typeof(A) == typeof(B);
+        list.ForEach(x => initial = func(initial, x));
+        return initial;
+    }
+}
---
+++ readme.md
@@ -0,0 +1,24 @@
+# Example question for Map/Reduce using C# generics
+
+Doing stuff, make it beetter
+
+- *Refactor the functions to further consolidate 
+
+## Installation
+
+Use Visual Studio Code 
+- Clone repo
+- Change .vscode Json if you wanna
+```
+    "console": "internalConsole", 
+    "stopAtEntry": false,
+```
+- Run debug 
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
